58c79eec8bafab1af2430fef39c4d67392a60f1e
hdang-dev <haidang.fordevelop@gmail.com>
2025-08-10 21:21:42 +0700

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import { Button } from "@/components/ui/button";
+import { Logo,  Button, Card, CardContent, Input } from "@/components";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col gap-4 items-center p-8">
-      <h1 className="font-bold">Quiz Dan</h1>
-      <span>Next.js</span>
-      <span>TailwindCSS</span>
-      <span>Shadcn/ui</span>
-      <Button>Done</Button>
+    <div style={{ backgroundImage: "url(home-background.svg)" }} className="size-full grid place-items-center bg-cover bg-center">
+      <div className="flex flex-col items-center gap-y-8">
+        <Logo />
+        <Card className="min-w-[350px]">
+          <CardContent>
+            <Input placeholder="Enter Room Code" className="text-center" />
+            <Button className="w-full mt-4">Join</Button>
+          </CardContent>
+        </Card>
+        <Link href="#" className="underline">
+          Create Quiz
+        </Link>
+      </div>
     </div>
   );
 }
